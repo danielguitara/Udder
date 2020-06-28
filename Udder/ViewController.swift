d3470382cd201ca3762a62832acf62d8c619b406
Daniel Guitara <danielguitara4@gmail.com>
2020-06-28 20:40:24 +0700

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Daniel Guitara on 28/06/20.
+//  Copyright © 2020 Daniel Guitara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
